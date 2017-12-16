759180c3f1b868a23e9f678b0c6078ca7f3e0e22
Jordan Bethea <jordan.bethea@gmail.com>
2017-12-15 22:37:10 -0500

    Adding initial Spring Boot setup

---
+++ src/main/java/CalendarDemo.java
@@ -0,0 +1,17 @@
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@EnableAutoConfiguration
+public class CalendarDemo {
+    
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(CalendarDemo.class, args);
+    }
+}
